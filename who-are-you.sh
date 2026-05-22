21d423f8529ebeac3dfadb0dbc94c8286f4929fb
Your Name <you@example.com>
2026-05-22 09:33:11 +0100

    whoareyou

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://acad.learn2earn.ng/assets/superhero/all.json | jq ".[] | select(id == 70) | .name"
+curl -s https://acad.learn2earn.ng/assets/superhero/all.json | jq ".[] | select(.id == 70) | .name"

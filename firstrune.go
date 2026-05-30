5914bf44b7adbc8494c0d1fc76790b6b5c344276
Your Name <you@example.com>
2026-05-30 15:00:31 +0100

    completed firstrune.go

--- firstrune.go
+++ firstrune.go
@@ -1,14 +1,6 @@
-package main
+package piscine
 
-import (
-		"piscine"
-
-		"github.com/01-edu/z01"
-)
-
-func main(){
-		z01.PrintRune(piscine.FirstRune("Hello!"))
-		z01.PrintRune(piscine.FirstRune("Salut!"))
-		z01.PrintRune(piscine.FirstRune("Ola!"))
-		z01.PrintRune('\n')
+func FirstRune(s string) rune {
+	str := []rune(s)
+	return str[0]
 }
